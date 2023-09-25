9586bb5de3ee47e078ff1f8f5483367364a1baaf
xyh <810169933@qq.com>
2023-09-25 09:24:14 +0800

    init

---
+++ src/main/java/com/se/fudan/recurrent/RecurrentApplication.java
@@ -0,0 +1,13 @@
+package com.se.fudan.recurrent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecurrentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecurrentApplication.class, args);
+    }
+
+}
